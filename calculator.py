f1aeec400ae2d170ed6387548077996730903737
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 15:16:09 -0700

    Added all math functions to calculator.

--- calculator.py
+++ calculator.py
@@ -2,15 +2,20 @@ import arithmetic
 
 def main():
    
-   while True:
-    user_input = raw_input()
-    user_tokens = user_input.split()
-    if user_tokens[0] == "q":
-        break
-    else:
-        num1 = int(user_tokens[1])
-        num2 = int(user_tokens[2])
-        print arithmetic.add(num1, num2)
+    symbols = {"+": arithmetic.add, "-": arithmetic.subtract, 
+    "*": arithmetic.multiply, "/": arithmetic.divide, "square": arithmetic.square,
+    "cube": arithmetic.cube, "pow": arithmetic.power, "mod": arithmetic.mod}
+
+    while True:
+        user_input = raw_input("> ")
+        user_tokens = user_input.split()
+        if user_tokens[0] == "q":
+            break
+        else:
+            sym = symbols[user_tokens[0]]
+            num1 = int(user_tokens[1])
+            num2 = int(user_tokens[2])
+            print sym(num1, num2)
 
 
 if __name__ == '__main__':
