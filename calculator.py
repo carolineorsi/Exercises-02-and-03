819e6bc1bc0c75149afee73f983f2215ab4a36ad
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 15:48:36 -0700

    Fixed cube and square, and checks length of user input.

--- calculator.py
+++ calculator.py
@@ -9,13 +9,22 @@ def main():
     while True:
         user_input = raw_input("> ")
         user_tokens = user_input.split()
+
         if user_tokens[0] == "q":
             break
-        else:
-            sym = symbols[user_tokens[0]]
-            num1 = float(user_tokens[1])
+
+        sym = symbols[user_tokens[0]]
+        num1 = float(user_tokens[1])
+
+        if len(user_tokens) == 2 and (user_tokens[0] == "square" or user_tokens[0] == "cube"):
+            print sym(num1)            
+
+        elif len(user_tokens) == 3:
             num2 = float(user_tokens[2])
             print sym(num1, num2)
+
+        else:
+            print "Wrong input!"
 
 
 if __name__ == '__main__':
