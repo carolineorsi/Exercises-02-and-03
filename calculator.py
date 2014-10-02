2933d028daaa9fbe68e4635da3771d8c18fe1e9a
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 17:08:11 -0700

    Made calculator work with more arguments, checks and validates.

--- calculator.py
+++ calculator.py
@@ -2,14 +2,19 @@ import arithmetic
 
 def main():
    
-    symbols = {"+": arithmetic.add, "-": arithmetic.subtract, 
-    "*": arithmetic.multiply, "/": arithmetic.divide, "square": arithmetic.square,
-    "cube": arithmetic.cube, "pow": arithmetic.power, "mod": arithmetic.mod}
+    symbols = {"+": arithmetic.add, 
+    "-": arithmetic.subtract, 
+    "*": arithmetic.multiply,
+    "/": arithmetic.divide, 
+    "square": arithmetic.square,
+    "cube": arithmetic.cube, 
+    "pow": arithmetic.power, 
+    "mod": arithmetic.mod}
 
     while True:
         user_input = raw_input("> ")
         user_tokens = user_input.split()
-
+       
         if user_tokens[0] == "q":
             break
         if user_tokens[0] not in symbols:
@@ -17,6 +22,7 @@ def main():
             continue
 
         sym = symbols[user_tokens[0]]
+
         try:
             num1 = float(user_tokens[1])
         except ValueError:
@@ -26,16 +32,17 @@ def main():
         if len(user_tokens) == 2 and (user_tokens[0] == "square" or user_tokens[0] == "cube"):
             print sym(num1)            
 
-        elif len(user_tokens) == 3:
-            try:
-                num2 = float(user_tokens[2])
-            except ValueError:
-                print("Not a valid input. Enter numbers.")
-                continue
-            print sym(num1, num2)
+        elif len(user_tokens) >= 3:
+            input_list = []
+            for item in user_tokens[1:]:
+                try:
+                    input_list.append(float(item))
+                except ValueError:
+                    print("Not a valid input. Enter numbers.")
+                    continue
+            print sym(*input_list)
 
-        else:
-            print "Not a valid input"
+
 
 
 if __name__ == '__main__':
